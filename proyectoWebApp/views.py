1c926caafe7009c76e4fc63986ab8d220267501b
luffy <luffy@onepiece.com>
2025-01-20 23:53:27 -0500

    Update 1.0

---
+++ proyectoWebApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    
+    return HttpResponse("Pagina HOME")
+
+def servicios(request):
+    
+    return HttpResponse("Pagina Servicios")
+
+def tienda(request):
+    
+    return HttpResponse("Pagina Tienda")
+
+def blog(request):
+    
+    return HttpResponse("Pagina blog")
+
+def contacto(request):
+    
+    return HttpResponse("Pagina contacto")
