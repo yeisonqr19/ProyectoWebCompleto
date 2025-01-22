f9f0555068aeb4d777c1da711ef52c01435612e2
luffy <luffy@onepiece.com>
2025-01-21 23:19:55 -0500

    Update 6.0

--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'proyectoWebApp',
+    'blogApp',
+    'contactoApp',
+    'serviciosApp',
+    'tiendaApp',
 ]
 
 MIDDLEWARE = [
