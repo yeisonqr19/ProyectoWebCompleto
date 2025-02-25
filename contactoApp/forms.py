69ed53d8d6648152eb771634a138e56ccc6c0760
luffy <luffy@hotmail.com>
2025-02-24 19:03:05 -0500

    Update 24-02-2025

--- contactoApp/forms.py
+++ contactoApp/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class ContactoForm(forms.Form):
     nombre = forms.CharField(label='Tu nombre', max_length=100, required=True)
     email = forms.EmailField(label='Tu Correo', required=True)
-    contenido = forms.CharField(label='Mensaje')
+    contenido = forms.CharField(label='Mensaje', widget= forms.Textarea)
     
-    
+
--- contactoApp/views.py
+++ contactoApp/views.py
@@ -7,9 +7,20 @@ def contacto(request):
     
     contacto_form = ContactoForm()
     
+    if request.method == "POST":
+        #Esto lo que hace es que carga la informacion que el usuario esta enviando por POST en el formulario
+        contacto_form = ContactoForm(data=request.POST)
+        
+        #Luego valido que el formulario que esta enviando el cliente este completamente lleno y que la informacion sea valida asi: 
+        if contacto_form.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            mensaje = request.POST.get("contenido")
+    
     ctx = {
         'contacto_form':contacto_form,
     }
     
     return render(request, "contactoApp/contacto.html", ctx)
+
 
