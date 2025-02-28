2aa55edc07965871cddceaaeca54bea815dee11d
luffy <luffy@hotmail.com>
2025-02-27 21:00:25 -0500

    subacategorias add

--- tiendaApp/models.py
+++ tiendaApp/models.py
@@ -7,7 +7,8 @@ class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=255, blank=True, null=True)
     
-    
+    #variable recursiva para que categoria pueda ser padre de otra categoria.
+    padre = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="subcategorias")
     
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -17,6 +18,13 @@ class Categoria(models.Model):
         verbose_name_plural = "categorias"
     
     def __str__(self):
+        return self.nombre
+    
+    def obtener_jerarquia(self):
+        
+        if self.padre:
+            return f"{self.padre.obtener_jerarquia()} > {self.nombre}"
+        
         return self.nombre
     
 class Producto(models.Model):
