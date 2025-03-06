bf561bdbe4be8909237c5a7b0c99a622ed99bfbe
luffy <luffy@hotmail.com>
2025-03-05 20:50:29 -0500

    Sub Categorias mostradas

--- tiendaApp/urls.py
+++ tiendaApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from tiendaApp import views
 
 urlpatterns = [
-    path('', views.tienda, name="tienda")
+    path('', views.tienda, name="tienda"),
+    
+    path('categoria/<int:categoria_id>/', views.productos_categorias, name="productos_por_categoria"),
+    
+    path('subcategoria/<int:subcategoria_id>/', views.productos_subcategorias, name="productos_por_subcategoria")
 ]
--- tiendaApp/views.py
+++ tiendaApp/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render
 from .models import Categoria, Producto
-
+from django.db.models import Q
 
 # Create your views here.
 def tienda(request):
     
     productos = Producto.objects.all()
-    
-    nombre_productos = [producto.nombre for producto in productos]
+    categorias = Categoria.objects.filter(padre__isnull=True)
     
     ctx = {
         'productos': productos,
+        'categorias': categorias,
+        'filtro': False,
     }
     
     return render(request,"tiendaApp/tienda.html", ctx)
+
+def productos_categorias(request, categoria_id):
+    categoria = Categoria.objects.get(id = categoria_id)
+    subcategorias = categoria.subcategorias.all()
+    
+    productos = Producto.objects.filter(
+        Q(categorias=categoria) | Q(categorias__in=subcategorias))   
+    
+    ctx = {
+        "productos": productos,
+        "categorias":Categoria.objects.filter(padre__isnull=True),
+        'filtro': True,
+    }
+    
+    return render(request, "tiendaApp/tienda.html", ctx)
+
+def productos_subcategorias(request, subcategoria_id):
+    
+    subcategoria = Categoria.objects.get(id =subcategoria_id)
+    productos = Producto.objects.filter(categorias=subcategoria)
+    
+    ctx = {
+        'productos': productos,
+        'categorias': Categoria.objects.filter(padre__isnull=True),
+        'filtro': True,
+    }
+    
+    return render(request, "tiendaApp/tienda.html", ctx)
