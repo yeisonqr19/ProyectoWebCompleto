03f6ec2299c10304d2173bf0097b8dd7a9ac27ca
luffy <luffy@hotmail.com>
2025-03-18 21:06:13 -0500

    View del carrito actualizado

--- carritoApp/views.py
+++ carritoApp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .carritoApp import CarritoApp
+from tiendaApp.models import Producto
 
 # Create your views here.
+
+def agregar_producto(request, producto_id):
+    
+    carro = CarritoApp(request)
+    
+    producto = Producto.objects.get(id = producto_id)
+    
+    #Utilizo el Metodo de agregar Producto de la clase:
+    carro.agregar_producto(producto = producto)
+    
+    return redirect("tienda")
+
+def eliminar_producto(request, producto_id):
+    
+    carro = CarritoApp(request)
+    
+    producto = Producto.objects.get(id = producto_id)
+    
+    carro.eliminar_producto(producto = producto)
+    
+    return redirect("tienda")
