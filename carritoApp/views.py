4ff38c4b9e57ba7f3b28ebbd6839fb8f1abd23be
luffy <luffy@hotmail.com>
2025-03-24 18:48:26 -0500

    carrito finalizado

--- carritoApp/views.py
+++ carritoApp/views.py
@@ -24,3 +24,21 @@ def eliminar_producto(request, producto_id):
     carro.eliminar_producto(producto = producto)
     
     return redirect("tienda")
+
+def restar_producto(request, producto_id):
+    
+    carro = CarritoApp(request)
+    
+    producto = Producto.objects.get(id = producto_id)
+    
+    carro.restar_producto(producto = producto)
+    
+    return redirect("tienda")
+
+def limpiar_carro(request):
+    
+    carro = CarritoApp(request)
+    
+    carro.limpiar_carro()    
+    
+    return redirect("tienda")
--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -75,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'proyectoWeb.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -85,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -104,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
