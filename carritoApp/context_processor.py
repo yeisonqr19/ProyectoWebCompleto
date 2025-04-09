f87f6141bd1cc09c0f9e094bf98d91e7f1ac1b1f
luffy <luffy@hotmail.com>
2025-04-08 20:35:59 -0500

    Context processor Creado

---
+++ carritoApp/context_processor.py
@@ -0,0 +1,10 @@
+
+def importe_total_carrito(request):
+    total = 0
+    
+    #Valido si el usuario esta Autenticado en mi Aplicacion:
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"]:
+            total = total + float(value["precio"] * value["cantidad"])
+            
+    return {"importe_total_carrito" : total}    
